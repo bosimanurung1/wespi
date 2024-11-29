de82b948d37d17dcbd1335556120c48ce280218d
bosimanurung1 <168368865+bosimanurung1@users.noreply.github.com>
2024-11-29 09:38:51 +0700

    Update new_calcb.py

--- new_calcb.py
+++ new_calcb.py
@@ -3,12 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from csv import writer
 
-# Your DataFrame with data to be inserted
-#df = pd.DataFrame(results, columns=['query', 'batch_index', 'index_of_audio_output_tensor', 'audio_file_name', 'similarity_score_by_model', 'user_relevance_score'])
-
-# Call the function to insert data into the Google Sheet
-#insert_data_into_sheet(df)
-
 tmycalc = pd.read_csv('tmycalc.csv')
 
 def new_calc_straight(): # from new_calc.py (in there, is counting Vogel)
