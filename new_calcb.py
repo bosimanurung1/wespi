3a0225e17af0c57a853c7c34e70a91af9f94b83e
bosimanurung1 <168368865+bosimanurung1@users.noreply.github.com>
2024-12-13 00:44:51 +0700

    Update new_calcb.py

--- new_calcb.py
+++ new_calcb.py
@@ -12,7 +12,8 @@ def new_calc_straight(): # from new_calc.py (in there, is counting Vogel)
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
     # Load the external CSS
-    css_path = pathlib.Path("assets/style.css")
+    #css_path = pathlib.Path("assets/style.css")
+    css_path = pathlib.Path("style.css")
     load_css(css_path)
 
     #Hitung2an Calculation sblm IPR Curve
